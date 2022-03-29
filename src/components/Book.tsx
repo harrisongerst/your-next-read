e77b0ed472f93e0c9170c7bbc87c4e3eb2d01b3e
harrisongers <hjgerstenlauer@gmail.com>
2022-03-29 16:48:51 -0400

    make button retrieve data from google books api

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 
-const handleSubmit = (e: React.FormEvent) => {
-  e.preventDefault();
-  console.log("we hittin");
-}
+
+
 
 export default function Book() {
+  const [book, setBook] = useState(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=flowers&key=AIzaSyCgGlkvNQgkSXMQSXdnenB2EoCnaAG2Z9c`)
+  .then(response => response.json())
+  .then(result => {
+    setBook(result)
+  })
+    console.log(book);
+  };
+  
   return (
     <div>
-    <form>
-    <button type="submit" onClick={handleSubmit}>Get Book</button>
-  </form>
-  </div>
-  )
+      <form>
+        <button type="submit" onClick={handleSubmit}>
+          Get Book
+        </button>
+      </form>
+    </div>
+  );
 }
