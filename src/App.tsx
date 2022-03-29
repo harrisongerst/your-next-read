22d9753a04bbfc481a9949c435166e1a511bbcf8
harrisongers <hjgerstenlauer@gmail.com>
2022-03-29 16:24:42 -0400

    add button to app and confirm functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,18 @@ interface AppProps {
   test: string
 }
 
+const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+  console.log("we hittin");
+}
+
 function App({test}: AppProps) {
   return (
     <div className="App">
       <h1>{test}</h1>
+      <form>
+        <button type="submit" onClick={handleSubmit}>Get Book</button>
+      </form>
     </div>
   );
 }
