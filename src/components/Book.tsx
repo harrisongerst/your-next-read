4d45c9197b53e098e4bf778c59f6f034bc2a028e
harrisongers <hjgerstenlauer@gmail.com>
2022-03-30 11:17:10 -0400

    add random book fetching to Get Book button

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -10,11 +10,14 @@ export default function Book() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const randomLetter = 's';
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${randomLetter}&key=AIzaSyCgGlkvNQgkSXMQSXdnenB2EoCnaAG2Z9c`)
+    const possibleChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const searchChar = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+    const randIndex = Math.floor(Math.random() * 50)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchChar}&startingIndex=${randIndex}&key=AIzaSyCgGlkvNQgkSXMQSXdnenB2EoCnaAG2Z9c`)
   .then(response => response.json())
   .then(result => {
-    console.log(result.items[1])
+    setBook(result.items[Math.floor(Math.random()*10)])
+    console.log(book)
   })
   };
   
