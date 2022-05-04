c0db6ed89fa84a052e0d6a43364c2776d9fb7aea
harrisongers <hjgerstenlauer@gmail.com>
2022-05-04 15:00:04 -0400

    change random char search to random word search

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -87,10 +87,11 @@ export default function Book() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const possibleChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    const searchChar = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+    const randomWords = require('random-words')
+    const searchWord = randomWords();
+    console.log(searchWord)
     const randIndex = Math.floor(Math.random() * 50)
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchChar}&startingIndex=${randIndex}&key=AIzaSyCgGlkvNQgkSXMQSXdnenB2EoCnaAG2Z9c`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchWord}&startingIndex=${randIndex}&key=AIzaSyCgGlkvNQgkSXMQSXdnenB2EoCnaAG2Z9c`)
   .then(response => response.json())
   .then(result => {
     setBook(result.items[Math.floor(Math.random()*10)])
