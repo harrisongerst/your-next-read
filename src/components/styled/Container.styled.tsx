a2c98a69401a5058a07e4dda2958a10e6147d7f1
harrisongers <hjgerstenlauer@gmail.com>
2022-05-06 17:28:43 -0400

    adjust grid layout, make info area scrollable

--- src/components/styled/Container.styled.tsx
+++ src/components/styled/Container.styled.tsx
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     display: grid;
-    grid-template-columns: .5fr 1fr;
-    grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: .3fr 1fr;
+    grid-template-rows: .8fr .2fr;
     grid-column-gap: 0px;
     grid-row-gap: 0px;
     justify-items: center;
--- src/components/styled/Info.styled.tsx
+++ src/components/styled/Info.styled.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
 export const Info = styled.div`
-    margin: 25px 50px 75px 100px;   
+    margin: 25px 50px 75px 100px; 
+    overflow-y:auto;  
 `
